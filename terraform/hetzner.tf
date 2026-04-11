8330a756aabe85cbf284069d7c64555fc5703380
Dan <github.qwixl@simplelogin.co>
2026-04-11 11:52:27 +0100

    fix(terraform): use cx23 server type (2 vCPU Intel, 4GB, available in fsn1)

--- terraform/hetzner.tf
+++ terraform/hetzner.tf
@@ -5,7 +5,7 @@ resource "hcloud_ssh_key" "unum" {
 
 resource "hcloud_server" "unum" {
   name        = "unum"
-  server_type = "cx21"
+  server_type = "cx23"
   image       = "ubuntu-24.04"
   location    = "fsn1"
   ssh_keys    = [hcloud_ssh_key.unum.id]
